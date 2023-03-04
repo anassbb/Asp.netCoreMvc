423efea491821f55701d1b6556a8c307fb881414
anassbb <anasabassi11@gmail.com>
2023-03-04 14:25:39 +0100

    Test For Commit

---
+++ MovieApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Hey you";
+        }
+    }
+}
